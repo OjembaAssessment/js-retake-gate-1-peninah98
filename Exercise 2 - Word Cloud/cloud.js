a71a3f5354105c2bfac7d548315666aee06719e2
peninah98 <peninangizwena@gmail.com>
2023-07-31 01:26:09 +0200

    Updates on word clod

--- Exercise 2 - Word Cloud/cloud.js
+++ Exercise 2 - Word Cloud/cloud.js
@@ -1,36 +1,42 @@
-// Function to generate a random color in hexadecimal format
+const paragraph = document.getElementById("myParagraph");
+const words = paragraph.innerText.toLowerCase().split(/\W+/);
+
 function getRandomColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+}
+
+const wordCount = {};
+for (const word of words) {
+  if (word.trim() !== "") {
+    wordCount[word] = (wordCount[word] || 0) + 1
   }
+}
+
+const wordFrequency = Object.entries(wordCount)
+
+wordFrequency.sort((a, b) => b[1] - a[1])
+
+const wordCloudContainer = document.getElementById("myWordCloud")
+
+let cloudHTML = ""
+const maxFontSize = 64
+
+const numWordsToDisplay = 12
+
+const cloudRadius = 100
+const rotationRange = 90 
+
+for (let i = 0; i < numWordsToDisplay; i++) {
+  const [word, count] = wordFrequency[i];
+  const fontSize = maxFontSize - i * 4;
+  const wordColor = getRandomColor();
+
   
-  const paragraph = document.getElementById("myParagraph");
-  const words = paragraph.innerText.toLowerCase().split(/\W+/);
-  
-  const wordCount = {};
-  for (const word of words) {
-    if (word.trim() !== "") {
-      wordCount[word] = (wordCount[word] || 0) + 1;
-    }
-  }
-  
-  const wordFrequency = Object.entries(wordCount);
-  
-  wordFrequency.sort((a, b) => b[1] - a[1]);
-  
-  const wordCloudContainer = document.getElementById("myWordCloud");
-  
-  let cloudHTML = "";
-  const maxFontSize = 64;
-  
-  for (let i = 0; i < Math.min(12, wordFrequency.length); i++) {
-    const [word, count] = wordFrequency[i];
-    const fontSize = maxFontSize - i * 4;
-  
-    // Generate a random color for each word
-    const wordColor = getRandomColor();
-  
-    cloudHTML += `<span style="font-size: ${fontSize}px; color: ${wordColor};">${word}</span> `;
-  }
-  
-  wordCloudContainer.innerHTML = cloudHTML;
-  
+  const angle = (Math.random() - 0.5) * rotationRange;
+  const x = cloudRadius * Math.cos((i / numWordsToDisplay) * Math.PI * 2 + angle);
+  const y = cloudRadius * Math.sin((i / numWordsToDisplay) * Math.PI * 2 + angle);
+
+  cloudHTML += `<span style="font-size: ${fontSize}px; color: ${wordColor}; transform: translate(${x}px, ${y}px) rotate(${angle}deg);">${word}</span> `;
+}
+
+wordCloudContainer.innerHTML = cloudHTML
